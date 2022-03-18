0a996b16e6f1203bacdde573c574b07cb47c2461
yeongno <dldudsh83@gmail.com>
2022-03-18 17:33:05 +0900

    initialization in constructor parameters

---
+++ example3.js
@@ -0,0 +1,9 @@
+"use strict";
+class Person2 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const p3 = new Person2("Mark", 39);
+console.log(p3);
---
+++ example3.ts
@@ -0,0 +1,9 @@
+class Person2{
+  public constructor(public name: string, public age?:number){
+    }
+  }
+
+
+const p3 = new Person2("Mark", 39);
+
+console.log(p3);
